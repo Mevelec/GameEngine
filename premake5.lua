d1d4b4b0a496e3ef5e285d647336efc258e0d4a7
mevelec <adrien.mevelec@hotmail.fr>
2019-10-17 12:12:12 +0200

    premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "GameEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GameEngine"
+	location "GameEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediates/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GE_PLATEFORM_WINDOWS",
+			"GE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "GE_DIST"
+		symbols "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediates/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GameEngine/vendor/spdlog/include",
+		"GameEngine/src"
+	}
+
+	links
+	{
+		"GameEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GE_PLATEFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "GE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GE_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "GE_DIST"
+		symbols "On"
