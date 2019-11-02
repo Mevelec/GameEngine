d9a65eadfde61d8bcebd666506c38174d38cb9c8
mevelec <adrien.mevelec@hotmail.fr>
2019-11-02 13:18:31 +0100

    revert premake

--- premake5.lua
+++ premake5.lua
@@ -72,7 +72,7 @@ project "GameEngine"
 
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} \" ../bin/" .. outputdir .. "/Sandbox/\"")
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
 		}
 
 	filter "configurations:Debug"
