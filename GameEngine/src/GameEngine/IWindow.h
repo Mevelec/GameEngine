5a8d68c23495a7fcc2e27690812949e4ed372825
mevelec <adrien.mevelec@hotmail.fr>
2019-11-06 17:28:17 +0100

    default destructor

--- GameEngine/src/GameEngine/IWindow.h
+++ GameEngine/src/GameEngine/IWindow.h
@@ -27,7 +27,7 @@ namespace GameEngine {
 	public:
 		using EventCallbackFn = std::function<void(Event&)>;
 
-		virtual ~IWindow() {}
+		virtual ~IWindow() = default;
 
 		virtual void onUpdate() = 0;
 
--- GameEngine/src/GameEngine/Layer/ILayer.cpp
+++ GameEngine/src/GameEngine/Layer/ILayer.cpp
@@ -7,9 +7,4 @@ namespace GameEngine {
 	{
 
 	}
-
-	ILayer::~ILayer()
-	{
-
-	}
 }
