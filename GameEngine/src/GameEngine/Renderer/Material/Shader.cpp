a02957c3ae6b8faed14e3cc3ea4f1070fdd2f7a0
mevelec <adrien.mevelec@hotmail.fr>
2019-12-16 14:42:15 +0100

    #29 shader lib

--- GameEngine/src/GameEngine/Renderer/Material/Shader.cpp
+++ GameEngine/src/GameEngine/Renderer/Material/Shader.cpp
@@ -6,31 +6,71 @@
 
 namespace GameEngine {
 
-	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
+	Ref<Shader> Shader::Create(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (IRenderer::GetAPI())
 		{
 			case RendererAPI::API::None:    
 				GE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
 			case RendererAPI::API::OpengGL:
-				return new OpenGLShader(vertexSrc, fragmentSrc);
+				return std::make_shared<OpenGLShader>(name, vertexSrc, fragmentSrc);
 		}
 
 		GE_CORE_ASSERT(false, "Unknow RendererAPI!");
 		return nullptr;
 	}
 
-	Shader* Shader::Create(const std::string& path)
+	Ref<Shader> Shader::Create(const std::string& path)
 	{
 		switch (IRenderer::GetAPI())
 		{
 		case RendererAPI::API::None:
 			GE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
 		case RendererAPI::API::OpengGL:
-			return new OpenGLShader(path);
+			return std::make_shared<OpenGLShader>(path);
 		}
 
 		GE_CORE_ASSERT(false, "Unknow RendererAPI!");
 		return nullptr;
+	}
+}
+
+namespace GameEngine {
+	void ShaderLibrary::add(const Ref<Shader>& shader)
+	{
+		auto& name = shader->getName();
+		GE_CORE_ASSERT(!this->exists(name), "Shader already exist");
+		this->shaders[name] = shader;
+	}
+
+	void ShaderLibrary::add(const std::string& name, const Ref<Shader>& shader)
+	{
+		GE_CORE_ASSERT(!this->exists(name), "Shader already exist");
+		this->shaders[name] = shader;
+	}
+
+	Ref<Shader> ShaderLibrary::load(const std::string& path)
+	{
+		auto shader = Shader::Create(path);
+		this->add(shader);
+		return shader;
+	}
+
+	Ref<Shader> ShaderLibrary::load(const std::string& name, const std::string& path)
+	{
+		auto shader = Shader::Create(path);
+		this->add(name, shader);
+		return shader;
+	}
+
+	Ref<Shader> ShaderLibrary::get(const std::string& name)
+	{
+		GE_CORE_ASSERT(this->exists(name), "Shader not found");
+		return this->shaders[name];
+	}
+
+	bool ShaderLibrary::exists(const std::string& name)
+	{
+		return this->shaders.find(name) != this->shaders.end();
 	}
 }
