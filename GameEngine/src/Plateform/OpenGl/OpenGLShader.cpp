b241d7444ace7f2b99fda32fd6313c10e32bf620
mevelec <adrien.mevelec@hotmail.fr>
2020-01-16 15:08:17 +0100

    Fixed edge case in OpenGLShader::PreProcess Logic

--- GameEngine/src/Plateform/OpenGl/OpenGLShader.cpp
+++ GameEngine/src/Plateform/OpenGl/OpenGLShader.cpp
@@ -77,22 +77,24 @@ namespace GameEngine {
 
 		const char* typeToken = "#type";
 		size_t typeTogenLength = strlen(typeToken);
-		size_t pos = source.find(typeToken, 0);
+		size_t pos = source.find(typeToken, 0); //Start of shader type declaration line
 		while (pos != std::string::npos)
 		{
-			size_t eol = source.find_first_of("\r\n", pos);
+			size_t eol = source.find_first_of("\r\n", pos); //End of shader type declaration line
 			GE_CORE_ASSERT(eol != std::string::npos, "Syntax error");
-			size_t begin = pos + typeTogenLength + 1;
+			size_t begin = pos + typeTogenLength + 1; //Start of shader type name (after "#type " keyword)
 			std::string type = source.substr(begin, eol - begin);
 			GE_CORE_ASSERT(shaderTypeFromString(type), "Invalid shader type specified");
 
-			size_t nextLinePos = source.find_first_of("\r\n", eol);
-			pos = source.find(typeToken, nextLinePos);
-			shaderSources[shaderTypeFromString(type)] = 
-				source.substr(
-					nextLinePos, 
-					pos - (nextLinePos == std::string::npos ? source.size() - 1 : nextLinePos)
-				);
+
+
+			size_t nextLinePos = source.find_first_not_of("\r\n", eol); //Start of shader code after shader type declaration line
+			GE_CORE_ASSERT(nextLinePos != std::string::npos, "Syntax error");
+			pos = source.find(typeToken, nextLinePos); //Start of next shader type declaration line
+
+			shaderSources[shaderTypeFromString(type)] = (pos == std::string::npos) ? 
+				source.substr(nextLinePos) : 
+				source.substr(nextLinePos, pos - nextLinePos);
 		}
 		return shaderSources;
 	}
