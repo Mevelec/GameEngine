1d0252c9b434c6a5d6159d045f9678ac8e8f17fc
mevelec <adrien.mevelec@hotmail.fr>
2020-01-16 14:48:15 +0100

    Automated platform detection using predefined macro's

--- GameEngine/src/GameEngine/Core/Core.h
+++ GameEngine/src/GameEngine/Core/Core.h
@@ -2,19 +2,60 @@
 
 #include <memory>
 
-#ifdef GE_PLATEFORM_WINDOWS
+#ifdef _WIN32
+	/* Windows x64/x86 */
+	#if _WIN64
+		/* Windows x64  */
+		#define GE_PLATFORM_WINDOWS
+	#else
+		/* Windows x86 */
+		#error "x86 Builds are not supported!"
+	#endif
+#elif defined(__APPLE__) || defined(__MACH__)
+	#include <TargetConditionals.h>
+	/* TARGET_OS_MAC exists on all the platforms
+	 * so we must check all of them (in this order)
+	 * to ensure that we're running on MAC
+	 * and not some other Apple platform */
+	#if TARGET_IPHONE_SIMULATOR == 1
+		#error "IOS simulator is not supported!"
+	#elif TARGET_OS_IPHONE == 1
+		#define GE_PLATFORM_IOS
+		#error "IOS is not supported!"
+	#elif TARGET_OS_MAC == 1
+		#define GE_PLATFORM_MACOS
+		#error "MacOS is not supported!"
+	#else
+		#error "Unknown Apple platform!"
+	#endif
+	 /* We also have to check __ANDROID__ before __linux__
+	  * since android is based on the linux kernel
+	  * it has __linux__ defined */
+#elif defined(__ANDROID__)
+	#define GE_PLATFORM_ANDROID
+	#error "Android is not supported!"
+#elif defined(__linux__)
+	#define GE_PLATFORM_LINUX
+	#error "Linux is not supported!"
+#else
+	/* Unknown compiler/platform */
+	#error "Unknown platform!"
+#endif // End of platform detection
+
+// DLL support
+#ifdef GE_PLATFORM_WINDOWS
 	#if GE_DYNAMIC_LINK
 		#ifdef GE_BUILD_DLL
-			#define  GE_API __declspec(dllexport)
+			#define GE_API __declspec(dllexport)
 		#else
-			#define  GE_API __declspec(dllimport)
-		#endif // GE_BUILD_DLL
+			#define GE_API __declspec(dllimport)
+		#endif
 	#else
 		#define GE_API
 	#endif
 #else
-	#error GameEngine only support Windows 
-#endif // GE_PLATEFORM_WINDOWS
+	#error Hazel only supports Windows!
+#endif // End of DLL support
 
 #ifdef GE_DEBUG
 	#define GE_ENABLE_ASSERTS
