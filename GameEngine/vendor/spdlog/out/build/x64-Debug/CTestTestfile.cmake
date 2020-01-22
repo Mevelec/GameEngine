c6ae238d2ed4495895531d0bcc5f7ededdef8e43
mevelec <adrien.mevelec@hotmail.fr>
2020-01-22 22:59:48 +0100

    update git ignore

--- GameEngine/vendor/spdlog/out/build/x64-Debug/CTestTestfile.cmake
+++ GameEngine/vendor/spdlog/out/build/x64-Debug/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: C:/Users/MEVELEC/Desktop/Project/GameEngine/GameEngine/vendor/spdlog
-# Build directory: C:/Users/MEVELEC/Desktop/Project/GameEngine/GameEngine/vendor/spdlog/out/build/x64-Debug
+# Source directory: D:/work/GameEngine/GameEngine/vendor/spdlog
+# Build directory: D:/work/GameEngine/GameEngine/vendor/spdlog/out/build/x64-Debug
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
